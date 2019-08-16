00d565c1d4e58c29be74f8a1c54dbc4c7405fdfa
K Hong <albtrossfacefall@gmail.com>
2019-08-16 09:24:28 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
       array.join
     when 2
       array.join(" and ")
-    when >=3
+    when array.length>=3
       array[-1]="and #{array[-1]}"
       array.join(", ")
   end
